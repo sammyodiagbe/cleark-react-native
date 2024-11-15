597488b74275df9c53be31f1feccf8c999447a97
Odiagbe Samson Osaro <sam.odiagbe.dev@gmail.com>
2024-11-15 14:18:36 -0500

    added user context and provider

--- app/home/_layout.tsx
+++ app/home/_layout.tsx
@@ -1,58 +1,59 @@
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import Feather from "@expo/vector-icons/Feather";
 import { Tabs } from "expo-router";
 import CustomTabBar from "@/components/customTabBar";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import UserProvider from "@/context/userProvider";
 export default function TabLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: "slateblue",
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: "transparent",
-          elevation: 0,
-        },
-      }}
-      tabBar={(props) => <CustomTabBar {...props} />}
-    >
-      <Tabs.Screen
-        name="(tabs)/index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <Ionicons size={28} name="home-sharp" color={color} />
-          ),
+    <UserProvider>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: "slateblue",
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: "transparent",
+            elevation: 0,
+          },
         }}
-      />
-      <Tabs.Screen
-        name="(tabs)/challenge"
-        options={{
-          title: "Challenge",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="flag-sharp" size={24} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="(tabs)/profile"
-        options={{
-          title: "Profile",
-          tabBarIcon: ({ color }) => (
-            <Ionicons size={28} name="person-sharp" color={color} />
-          ),
-        }}
-      />
+        tabBar={(props) => <CustomTabBar {...props} />}
+      >
+        <Tabs.Screen
+          name="(tabs)/index"
+          options={{
+            title: "Home",
+            tabBarIcon: ({ color }) => (
+              <Ionicons size={28} name="home-sharp" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="(tabs)/challenge"
+          options={{
+            title: "Challenge",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="flag-sharp" size={24} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="(tabs)/profile"
+          options={{
+            title: "Profile",
+            tabBarIcon: ({ color }) => (
+              <Ionicons size={28} name="person-sharp" color={color} />
+            ),
+          }}
+        />
 
-      <Tabs.Screen
-        name="(tabs)/wallet"
-        options={{
-          title: "Wallet",
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="wallet-sharp" size={24} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+        <Tabs.Screen
+          name="(tabs)/wallet"
+          options={{
+            title: "Wallet",
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="wallet-sharp" size={24} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </UserProvider>
   );
 }
